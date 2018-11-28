b89229ec6a317871e0c0dfeae7ba90e24d231577
blues <470960070@qq.com>
2018-11-28 20:25:46 +0800

    用户鉴权系统

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,7 +1,7 @@
 from app import db
 from app.auth import bp
-from flask_jwt import jwt_required,current_identity
-from flask import jsonify,request
+from flask_jwt import jwt_required, current_identity
+from flask import jsonify, request
 from app.models import User
 
 
@@ -12,12 +12,13 @@ def register():
     if username is None or password is None:
         return jsonify({'error': '请输入用户名和密码'}), 499
     if User.query.filter_by(username=username).first() is not None:
-        return jsonify({'error': '用户已注册'}) , 499
+        return jsonify({'error': '用户已注册'}), 499
     user = User(username)
     user.set_password(password)
     db.session.add(user)
     db.session.commit()
     return jsonify({'username': user.username}), 201
+
 
 @bp.route('/user', methods=['GET'])
 @jwt_required()
@@ -28,4 +29,3 @@ def current_user():
         'email': current_identity.email,
         'mobile': current_identity.mobile
     }), 201
-
--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), index=True, unique=True)
